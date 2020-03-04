ea9cb3eafce22852ff5254ccc8e3b1623f930d16
faiz alkautsar <alkafaiz99@gmail.com>
2020-03-05 01:45:22 +0800

    [CREATE] initialize add image upload feature

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,17 @@
 import React, { useState, useEffect, useRef } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import classnames from "classnames";
-import { Box, Typography, TextField, Button } from "@material-ui/core";
+import {
+  Box,
+  Typography,
+  TextField,
+  Button,
+  IconButton
+} from "@material-ui/core";
 import PostAddIcon from "@material-ui/icons/PostAdd";
 import { createMoment } from "../firebase";
+import ImageRoundedIcon from "@material-ui/icons/ImageRounded";
+import CancelRoundedIcon from "@material-ui/icons/CancelRounded";
 
 const style = theme => ({
   wrapper: {
@@ -27,6 +35,48 @@ const style = theme => ({
     color: "white",
     borderRadius: 50,
     fontFamily: "Montserrat"
+  },
+  buttonContainer: {
+    display: "flex",
+    justifyContent: "space-between"
+  },
+  imageContainer: {
+    height: 150,
+    width: "100%",
+    backgroundColor: theme.palette.grey[100],
+    display: "flex",
+    alignItems: "center"
+  },
+  imageItem: {
+    height: 130,
+    width: 130,
+    backgroundColor: theme.palette.grey[300],
+    marginLeft: 10,
+    position: "relative",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+    "& img": {
+      minHeight: "100%",
+      maxWidth: "100%",
+      flexShrink: 0,
+      objectFit: "cover"
+    }
+  },
+  imageClose: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    transition: "all ease-in-out .3s",
+    color: theme.palette.grey[700],
+    "&:hover": {
+      color: theme.palette.grey[500]
+    }
+  },
+  imageUpload: {},
+  inputImage: {
+    display: "none"
   }
 });
 
@@ -61,10 +111,13 @@ function getDate() {
 
 export default function Form(props) {
   const classes = useStyle();
+  const inputImgRef = useRef();
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [err, setErr] = useState(false);
   const { id, email } = props;
+  const [uploadImages, setUploadImages] = useState([]);
+
   function clearForm() {
     setTitle("");
     setBody("");
@@ -88,6 +141,28 @@ export default function Form(props) {
     };
     createMoment(obj);
     clearForm();
+  };
+
+  const handleImgChange = e => {
+    const file = inputImgRef.current.files[0];
+    const fr = new FileReader();
+    if (file) {
+      const image = URL.createObjectURL(e.target.files[0]);
+      console.log("imput image ref", e.target.files[0]);
+      setUploadImages(ps => [...ps, image]);
+    }
+  };
+
+  const ImageItemUpload = props => {
+    const { img } = props;
+    return (
+      <Box className={classes.imageItem}>
+        <IconButton className={classes.imageClose} size="small">
+          <CancelRoundedIcon />
+        </IconButton>
+        <img className={classes.imageUpload} src={img} alt="" />
+      </Box>
+    );
   };
 
   return (
@@ -115,7 +190,7 @@ export default function Form(props) {
           onChange={e => setTitle(e.currentTarget.value)}
         />
       </Box>
-      <Box mt={3} mb={4}>
+      <Box mt={3} mb={2}>
         <TextField
           InputProps={{
             classes: {
@@ -133,19 +208,51 @@ export default function Form(props) {
           onChange={e => setBody(e.currentTarget.value)}
         />
       </Box>
-      <Button
-        className={classes.button}
-        variant="contained"
-        disableElevation
-        disabled={err}
-        startIcon={<PostAddIcon />}
-        onClick={e => {
-          e.preventDefault();
-          handleAdd();
-        }}
-      >
-        Add
-      </Button>
+      <Box className={classes.imageContainer} mb={3}>
+        {console.log(uploadImages)}
+        {uploadImages.length !== 0
+          ? uploadImages.map((data, index) => {
+              console.log(data);
+              return <ImageItemUpload key={index} img={data} />;
+            })
+          : ""}
+      </Box>
+      <Box className={classes.buttonContainer}>
+        <input
+          accept="image/*"
+          className={classes.inputImage}
+          id="contained-button-file"
+          multiple
+          type="file"
+          ref={inputImgRef}
+          onChange={handleImgChange}
+        />
+        <label htmlFor="contained-button-file">
+          <Button
+            component="span"
+            className={classes.button}
+            variant="contained"
+            disableElevation
+            startIcon={<ImageRoundedIcon />}
+          >
+            Add Image
+          </Button>
+        </label>
+
+        <Button
+          className={classes.button}
+          variant="contained"
+          disableElevation
+          disabled={err}
+          startIcon={<PostAddIcon />}
+          onClick={e => {
+            e.preventDefault();
+            handleAdd();
+          }}
+        >
+          Post Moment
+        </Button>
+      </Box>
     </Box>
   );
 }
